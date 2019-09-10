b226ca60793732f60b6ede4acb5028014b2c34ca
tomjeans <37204571+tomjeans@users.noreply.github.com>
2019-09-10 12:22:28 +0800

    add the utral datasets!

--- src/lib/datasets/dataset_factory.py
+++ src/lib/datasets/dataset_factory.py
@@ -11,13 +11,14 @@ from .dataset.coco import COCO
 from .dataset.pascal import PascalVOC
 from .dataset.kitti import KITTI
 from .dataset.coco_hp import COCOHP
-
+from .dataset.utral import Utral
 
 dataset_factory = {
   'coco': COCO,
   'pascal': PascalVOC,
   'kitti': KITTI,
-  'coco_hp': COCOHP
+  'coco_hp': COCOHP,
+  'utral': Utral
 }
 
 _sample_factory = {
